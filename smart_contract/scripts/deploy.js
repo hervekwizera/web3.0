beead7cd332d4d629e941944653441ca85ad66ae
hervekwizera <hervekwizera63@gmail.com>
2025-07-30 23:07:02 +0200

    writting code for  deploy.js

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -2,18 +2,18 @@ const main = async () => {
     const Transactions = await hre.ethers.getContractFactory("Transactions");
     const transactions = await Transactions.deploy();
 
-    await transactions.deployed;
-    console.log("Transactions deployed to: ", transactions.address);
+    await transactions.deployed(); // corrected here
+    console.log("Transactions deployed to:", transactions.address);
+};
 
-    const runMain = async () =>{
-        try {
-            await main();
-            Process.exit(0)
-        } catch (error) {
-            console.error(error);
-            process.exit(1);
-        }
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0); // lowercase 'p'
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
     }
-}
+};
 
- runMain();
+runMain(); // moved outside
