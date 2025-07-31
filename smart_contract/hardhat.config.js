420553f26a906f73ecc664af93112195811479c4
hervekwizera <hervekwizera63@gmail.com>
2025-07-31 21:49:10 +0200

    modifing code for  deploy.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    Sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/lSI1VhpcJKhDH2nZJjIJa',
+      accounts: ['257e61363128d3935c0dcfc6e55a5704a2ec8f3f46ae6aa88400460e812368ac'],
     },
   },
 };
