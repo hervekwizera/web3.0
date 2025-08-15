5e6bdc4ebbab5a22828d5526d77d1edba97b03c1
hervekwizera <hervekwizera63@gmail.com>
2025-08-15 12:51:39 +0200

    setting up env

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,12 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/lSI1VhpcJKhDH2nZJjIJa',
-      accounts: ['ba69ba0f2568b312737fb9d2c58c0085a227bbace9f247f2808e87f22ca40f77'],
+      url: process.env.ALCHEMY_URL, // Alchemy RPC endpoint
+      accounts: [process.env.PRIVATE_KEY], // Private key from .env file
     },
   },
 };
